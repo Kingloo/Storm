0bac6911fbd6f4fafe2741095934944f8459b332
Kingloo <k1ngloo@hotmail.com>
2016-09-23 03:51:18 +0200

    Change WebRequest namespace
    Removing .Extensions means we don't need the using.

--- Storm/Extensions/WebRequest.cs
+++ Storm/Extensions/WebRequest.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 
-namespace Storm.Extensions
+namespace Storm
 {
     public static class WebRequestExt
     {
--- Storm/Utils.cs
+++ Storm/Utils.cs
@@ -8,7 +8,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
-using Storm.Extensions;
 
 namespace Storm
 {
