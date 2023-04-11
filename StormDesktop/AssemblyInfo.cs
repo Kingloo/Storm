db9ad8006c0b75fbab3375cd19cf6c48f489fc8c
Kingloo <k1ngloo@hotmail.com>
2023-04-11 06:49:32 +0200

    Update AssemblyInfo files for StormDesktop and StormLib

--- StormDesktop/AssemblyInfo.cs
+++ StormDesktop/AssemblyInfo.cs
@@ -1,2 +1,8 @@
 using System;
+using System.Reflection;
+
 [assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("StormDesktop")]
+[assembly: AssemblyProduct("Storm")]
+[assembly: AssemblyCopyright("© Kingloo 2023")]
+[assembly: AssemblyDescription("Windows desktop programme for following livestreams across different services. No account needed!")]
--- StormLib/AssemblyInfo.cs
+++ StormLib/AssemblyInfo.cs
@@ -1,2 +1,8 @@
 using System;
+using System.Reflection;
+
 [assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("StormLib")]
+[assembly: AssemblyProduct("Storm")]
+[assembly: AssemblyCopyright("© Kingloo 2023")]
+[assembly: AssemblyDescription("Library to communicate with all the streaming services.")]
