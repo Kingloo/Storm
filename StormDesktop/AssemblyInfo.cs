95ad216ecbaa919e0edc95e1c6ae29fdc2e9cf67
Kingloo <k1ngloo@hotmail.com>
2023-04-11 06:58:39 +0200

    Remove some things from AssemblyInfo files

--- StormDesktop/AssemblyInfo.cs
+++ StormDesktop/AssemblyInfo.cs
@@ -2,7 +2,4 @@ using System;
 using System.Reflection;
 
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyTitle("StormDesktop")]
-[assembly: AssemblyProduct("Storm")]
-[assembly: AssemblyCopyright("© Kingloo 2023")]
 [assembly: AssemblyDescription("Windows desktop programme for following livestreams across different services. No account needed!")]
--- StormLib/AssemblyInfo.cs
+++ StormLib/AssemblyInfo.cs
@@ -2,7 +2,5 @@ using System;
 using System.Reflection;
 
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyTitle("StormLib")]
-[assembly: AssemblyProduct("Storm")]
 [assembly: AssemblyCopyright("© Kingloo 2023")]
 [assembly: AssemblyDescription("Library to communicate with all the streaming services.")]
