1d1c1b445f409c461f3e72f62815829fcfeffc5b
Kingloo <k1ngloo@hotmail.com>
2023-12-11 22:08:10 +0100

    Project files and launch.json

--- StormLib/AssemblyInfo.cs
+++ StormLib/AssemblyInfo.cs
@@ -2,5 +2,4 @@ using System;
 using System.Reflection;
 
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyCopyright("© Kingloo 2023")]
 [assembly: AssemblyDescription("Library to communicate with all the streaming services.")]
--- publish-desktop-x64.ps1
+++ publish-desktop-x64.ps1
@@ -1,3 +1,3 @@
 dotnet restore
 dotnet build .\Storm.sln -c Release --no-restore --nologo
-dotnet publish .\StormDesktop\StormDesktop.csproj -c Release -r win10-x64 /p:PublishSingleFile=true --no-self-contained --no-restore
+dotnet publish .\StormDesktop\StormDesktop.csproj -c Release -r win-x64 /p:PublishSingleFile=true --no-self-contained --no-restore
