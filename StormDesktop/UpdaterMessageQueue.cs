84969236cd44879728ef197b836be6bf3a29dd63
Kingloo <k1ngloo@hotmail.com>
2025-12-31 06:14:49 +0100

    Remove unused method

--- StormDesktop/UpdaterMessageQueue.cs
+++ StormDesktop/UpdaterMessageQueue.cs
@@ -15,18 +15,15 @@ namespace StormDesktop
 	public class UpdaterMessageQueue
 	{
 		private readonly ILogger<UpdaterMessageQueue> logger;
-		private readonly IServiceProvider serviceProvider;
 
 		public ConcurrentQueue<object> ResultsQueue { get; } = new ConcurrentQueue<object>();
 		public IReadOnlyCollection<IStream> StreamSource { get; set; } = new Collection<IStream>();
 
-		public UpdaterMessageQueue(ILogger<UpdaterMessageQueue> logger, IServiceProvider serviceProvider)
+		public UpdaterMessageQueue(ILogger<UpdaterMessageQueue> logger)
 		{
 			ArgumentNullException.ThrowIfNull(logger);
-			ArgumentNullException.ThrowIfNull(serviceProvider);
 
 			this.logger = logger;
-			this.serviceProvider = serviceProvider;
 		}
 
 		public void SetStreamSource(IReadOnlyCollection<IStream> streamSource)
@@ -39,25 +36,6 @@ namespace StormDesktop
 				streamSource.Count == 1 ? "item" : "items");
 
 			StreamSource = streamSource;
-		}
-
-		public async Task UpdateAsync<TStream>(IReadOnlyList<TStream> streams, CancellationToken cancellationToken) where TStream : notnull, IStream
-		{
-			ArgumentNullException.ThrowIfNull(streams);
-
-			if (!streams.Any())
-			{
-				return;
-			}
-
-			IUpdater<TStream> updater = serviceProvider.GetRequiredService<IUpdater<TStream>>();
-
-			IReadOnlyList<Result<TStream>> results = await updater.UpdateAsync(streams, cancellationToken).ConfigureAwait(false);
-
-			foreach (Result<TStream> result in results)
-			{
-				ResultsQueue.Enqueue(result);
-			}
 		}
 	}
 }
